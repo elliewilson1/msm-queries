41632e75b02722dabffd3a4fc3a889a39f022f78
elliewilson1 <ewilson0@chicagobooth.edu>
2025-04-22 21:37:45 +0000

    directors done

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get("/directors", { :controller => "directors", :action => "index" })
 
+  get("/directors/:the_id", { :controller => "directors", :action => "show" })
+
   get("/movies", { :controller => "movies", :action => "index" })
 
   get("/actors", { :controller => "actors", :action => "index" })
